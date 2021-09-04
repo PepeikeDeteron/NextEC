aa3d8b843f1196e469381042082b85070c5a1673
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-09-05 01:17:28 +0900

    Material-UI, styled-components を導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import Head from 'next/head';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { StylesProvider, MuiThemeProvider } from '@material-ui/core/styles';
+import 'normalize.css';
+import '../styles/globals.css';
+import theme from '@/styles/theme';
 
-const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
+const App: React.VFC<AppProps> = ({
+  Component,
+  pageProps,
+}: AppProps): JSX.Element => {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
+      <StylesProvider injectFirst>
+        <MuiThemeProvider theme={theme}>
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </MuiThemeProvider>
+      </StylesProvider>
     </>
   );
 };
