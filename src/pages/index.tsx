b301a794da22ad97ac9132d0a9b8bd3fb4a767fb
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-09-06 19:46:39 +0900

    Redux-Toolkit の簡単な動作確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import TestButtonList from '@/components/molecules/TestButtonList';
 
+import { useDispatch } from 'react-redux';
+import { signIn } from '@/modules/users/usersSlice';
+
 const Home: React.VFC = () => {
+  const dispatch = useDispatch();
+
   return (
     <>
       <TestButtonList />
+
+      <button
+        onClick={() => dispatch(signIn({ uid: '0001', username: 'user' }))}
+      >
+        Sign In
+      </button>
     </>
   );
 };
