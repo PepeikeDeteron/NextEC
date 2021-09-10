4b8b571128386c2a9f24f387e3caf0429c55e65a
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-09-10 20:03:00 +0900

    サインイン画面を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignUp from '@/components/templates/SignUp';
+import SignIn from '@/components/templates/SignIn';
 
 const Home: React.VFC = () => {
   return (
     <>
       <SignUp />
+      <SignIn />
     </>
   );
 };
