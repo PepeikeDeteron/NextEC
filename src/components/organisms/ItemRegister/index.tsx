cd0bf5d2ca6218e53393807b49a3c8e00a7f5832
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-09-13 19:51:47 +0900

    スピンボタンを非表示

--- src/components/organisms/ItemRegister/index.tsx
+++ src/components/organisms/ItemRegister/index.tsx
@@ -113,6 +113,13 @@ const StyledComponent = styled(Component)`
     margin: 0 auto;
     text-align: center;
   }
+
+  input[type='number']::-webkit-outer-spin-button,
+  input[type='number']::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+    -moz-appearance: textfield;
+  }
 `;
 
 const Container: React.VFC<ContainerProps> = () => {
